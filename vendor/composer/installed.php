d1ec4545de69c18ed97bec0d113937d3bfaabe3b
George Nicolaou <orionas.elite@gmail.com>
2025-10-16 05:51:45 +0300

    composer updater install

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'f79379e35835645166c64f89ac68218ca5a9a322',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'f79379e35835645166c64f89ac68218ca5a9a322',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'content-pilot/wp-plugin-update-checker' => array(
+            'pretty_version' => '2.0.0',
+            'version' => '2.0.0.0',
+            'reference' => 'e5e54715d8f3e12a1aff3ed640c50c1cb07046fd',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../content-pilot/wp-plugin-update-checker',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
